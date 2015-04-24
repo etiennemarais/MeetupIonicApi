b6bc82c308c343b6e5309e511dd20b438f009440
Etienne Marais <etienne.m@everlytic.com>
2015-04-24 15:53:59 +0200

    Answers post - To hard code the response for demo sake

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 use App\Services\Askme\Facades\Question;
+use Askme\Domain\Recommendations\Recommend;
 use EllipseSynergie\ApiResponse\Laravel\Response;
 
 class WelcomeController extends ApiController
@@ -33,5 +34,27 @@ class WelcomeController extends ApiController
 			'status' => 'success',
 			'data' => $questions,
 		]);
+	}
+
+	/**
+	 * @return \Illuminate\Contracts\Routing\ResponseFactory
+	 */
+	public function postAnswers()
+	{
+		$recommends = Recommend::All();
+
+		$data = [
+			'status' => 'success',
+			'data' => [
+				'suggestion' => $recommends[0],
+				'alternatives' => [
+					$recommends[1],
+					$recommends[2],
+					$recommends[3],
+				],
+			],
+		];
+
+		return $this->response->withArray($data);
 	}
 }
